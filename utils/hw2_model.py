049727216aaa17c0daabd78356cef0d33fcaeabf
Artem Kotov <breengles@gmail.com>
2021-11-09 00:10:48 +0300

    lots of fixes: masks, added grad clip

--- utils/hw2_model.py
+++ utils/hw2_model.py
@@ -6,60 +6,39 @@ from tqdm.auto import trange, tqdm
 
 
 class MaskedConv(nn.Conv2d):
-    def __init__(self, dc=3, *args, **kwargs):
+    def __init__(self, color=True, dc=3, isB=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.register_buffer("mask", torch.zeros_like(self.weight))
-        self.set_mask(dc)
 
-    def _set_spatial_mask(self, isB=False):
-        h, w = self.kernel_size
-        self.mask[:, :, h // 2, : w // 2 + isB] = 1
-        self.mask[:, :, : h // 2] = 1
+        self.color = (color,)
+        self.dc = dc
+        self.isB = isB
 
-    def _set_channel_mask(self, dc, isB=False):
+        self.set_mask()
+
+    def set_mask(self):
         outc, inc, h, w = self.weight.shape
-        self.mask[:, :, h // 2, w // 2] = self._mask_channel(inc, outc, dc, isB)
 
-    def _mask_channel(self, inc, outc, dc, isB=False):
-        inf = inc // dc + 1
-        outf = outc // dc + 1
+        if self.color:
+            self.mask[:, :, h // 2, : w // 2] = 1
+            self.mask[:, :, : h // 2] = 1
 
-        mask = torch.tril(torch.ones((dc, dc)), isB - 1)
+            group_out, group_in = outc // self.dc, inc // self.dc
 
-        # duplicating
-        mask = torch.cat([mask] * inf, dim=1)
-        mask = torch.cat([mask] * outf, dim=0)
-
-        return mask[:outc, :inc]
+            if self.isB:
+                self.mask[:group_out, :group_in, h // 2, w // 2] = 1
+                self.mask[group_out : 2 * group_out, : 2 * group_in, h // 2, w // 2] = 1
+                self.mask[2 * group_out :, :, h // 2, w // 2] = 1
+            else:
+                self.mask[group_out : 2 * group_out, :group_in, h // 2, w // 2] = 1
+                self.mask[2 * group_out :, : 2 * group_in, h // 2, w // 2] = 1
+        else:
+            self.mask[:, :, h // 2, : w // 2 + self.isB] = 1
+            self.mask[:, :, : h // 2] = 1
 
     def forward(self, x):
         self.weight.data = self.weight.data.mul(self.mask)  # applying mask
         return super().forward(x)  # call regular Conv2d
-
-    def set_mask(self, dc, isB=False):
-        raise NotImplementedError()
-
-
-class ConvA(MaskedConv):
-    def set_mask(self, dc):
-        self._set_spatial_mask(False)
-        self._set_channel_mask(dc, False)
-
-
-class ConvB(MaskedConv):
-    def set_mask(self, dc):
-        self._set_spatial_mask(True)
-        self._set_channel_mask(dc, True)
-
-
-class SpatialConvA(MaskedConv):
-    def set_mask(self, dc):
-        self._set_spatial_mask(False)
-
-
-class SpatialConvB(MaskedConv):
-    def set_mask(self, dc):
-        self._set_spatial_mask(True)
 
 
 class ResidualBlock(nn.Module):
@@ -67,29 +46,21 @@ class ResidualBlock(nn.Module):
         super().__init__()
         h = inc // 2
 
-        if color:
-            self.main = nn.Sequential(
-                ConvB(in_channels=inc, out_channels=h, kernel_size=1),
-                nn.ReLU(),
-                ConvB(in_channels=h, out_channels=h, kernel_size=7, padding=3),
-                nn.ReLU(),
-                ConvB(in_channels=h, out_channels=inc, kernel_size=1),
-            )
-        else:
-            self.main = nn.Sequential(
-                SpatialConvB(in_channels=inc, out_channels=h, kernel_size=1),
-                nn.ReLU(),
-                SpatialConvB(in_channels=h, out_channels=h, kernel_size=7, padding=3),
-                nn.ReLU(),
-                SpatialConvB(in_channels=h, out_channels=inc, kernel_size=1),
-            )
+        self.main = nn.Sequential(
+            nn.ReLU(),
+            MaskedConv(color=color, dc=3, isB=True, in_channels=inc, out_channels=h, kernel_size=1),
+            nn.ReLU(),
+            MaskedConv(color=color, dc=3, isB=True, in_channels=h, out_channels=h, kernel_size=7, padding=3),
+            nn.ReLU(),
+            MaskedConv(color=color, dc=3, isB=True, in_channels=h, out_channels=inc, kernel_size=1),
+        )
 
     def forward(self, x):
         return self.main(x) + x
 
 
-class IndependentPixelCNN(nn.Module):
-    def __init__(self, input_shape, cf=120, num_classes=4):
+class PixelCNN(nn.Module):
+    def __init__(self, input_shape, cf=120, num_classes=4, color=True):
         super().__init__()
         h, w, c = input_shape
         self.input_shape = (h, w)
@@ -97,37 +68,28 @@ class IndependentPixelCNN(nn.Module):
         self.num_classes = num_classes
 
         self.model = nn.Sequential(
-            SpatialConvA(in_channels=c, out_channels=cf, kernel_size=7, padding=3),
+            MaskedConv(color=color, dc=3, isB=False, in_channels=c, out_channels=cf, kernel_size=7, padding=3),
             nn.ReLU(),
-            ResidualBlock(cf, False),
+            ResidualBlock(cf, color),
+            ResidualBlock(cf, color),
+            ResidualBlock(cf, color),
+            ResidualBlock(cf, color),
+            ResidualBlock(cf, color),
+            ResidualBlock(cf, color),
+            ResidualBlock(cf, color),
+            ResidualBlock(cf, color),
+            MaskedConv(color=color, dc=3, isB=True, in_channels=cf, out_channels=cf, kernel_size=1),
             nn.ReLU(),
-            ResidualBlock(cf, False),
-            nn.ReLU(),
-            ResidualBlock(cf, False),
-            nn.ReLU(),
-            ResidualBlock(cf, False),
-            nn.ReLU(),
-            ResidualBlock(cf, False),
-            nn.ReLU(),
-            ResidualBlock(cf, False),
-            nn.ReLU(),
-            ResidualBlock(cf, False),
-            nn.ReLU(),
-            ResidualBlock(cf, False),
-            nn.ReLU(),
-            SpatialConvB(in_channels=cf, out_channels=cf, kernel_size=1),
-            nn.ReLU(),
-            SpatialConvB(in_channels=cf, out_channels=c * num_classes, kernel_size=1),
+            MaskedConv(color=color, dc=3, isB=True, in_channels=cf, out_channels=c * num_classes, kernel_size=1),
         )
 
     def forward(self, x):
-        return self.model(x)
+        # return self.model(x).reshape(x.shape[0], self.num_classes, self.dc, *self.input_shape)
+        return self.model(x).reshape(x.shape[0], self.dc, self.num_classes, *self.input_shape).permute(0, 2, 1, 3, 4)
 
     def predict_proba(self, x):
         with torch.no_grad():
-            outs = self(x).reshape(x.shape[0], self.num_classes, self.dc, *self.input_shape)
-            proba = F.softmax(outs, dim=1)
-            return proba
+            return F.softmax(self(x), dim=1)
 
     @property
     def device(self):
@@ -135,9 +97,7 @@ class IndependentPixelCNN(nn.Module):
 
     def __step(self, batch):
         batch = batch.to(self.device)
-        outs = self(batch).reshape(batch.shape[0], self.num_classes, self.dc, *self.input_shape)
-        loss = F.cross_entropy(outs, batch.long())
-        return loss
+        return F.cross_entropy(self(batch), batch.long())
 
     def __test(self, testloader):
         losses = []
@@ -163,6 +123,7 @@ class IndependentPixelCNN(nn.Module):
 
                 optim.zero_grad()
                 loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.parameters(), 1)
                 optim.step()
 
                 train_losses.append(loss.detach().cpu().numpy())
@@ -172,64 +133,13 @@ class IndependentPixelCNN(nn.Module):
 
         return self, losses
 
-    def sample(self):
+    def sample(self, n):
+        sample = torch.zeros(n, self.dc, *self.input_shape).to(self.device)
         with torch.no_grad():
-            sample = torch.zeros(1, self.dc, *self.input_shape).to(self.device)
-
-            for i in range(self.input_shape[0]):
-                for j in range(self.input_shape[1]):
-                    probs = self.predict_proba(sample)[0, :, :, i, j].cpu().numpy()
-
-                    labels = []
-                    for c in range(self.dc):
-                        labels.append(np.random.choice(self.num_classes, p=probs[:, c]))
-                    labels = torch.tensor(labels, dtype=int)
-
-                    sample[0, :, i, j] = labels
-
-        return sample[0].cpu().numpy().transpose(1, 2, 0)
-
-
-class PixelCNN(IndependentPixelCNN):
-    def __init__(self, input_shape, cf=120, num_classes=4):
-        super().__init__(input_shape, cf, num_classes)
-        h, w, c = input_shape
-        self.input_shape = (h, w)
-        self.dc = c
-        self.num_classes = num_classes
-
-        self.model = nn.Sequential(
-            ConvA(in_channels=c, out_channels=cf, kernel_size=7, padding=3),
-            nn.ReLU(),
-            ResidualBlock(cf),
-            nn.ReLU(),
-            ResidualBlock(cf),
-            nn.ReLU(),
-            ResidualBlock(cf),
-            nn.ReLU(),
-            ResidualBlock(cf),
-            nn.ReLU(),
-            ResidualBlock(cf),
-            nn.ReLU(),
-            ResidualBlock(cf),
-            nn.ReLU(),
-            ResidualBlock(cf),
-            nn.ReLU(),
-            ResidualBlock(cf),
-            nn.ReLU(),
-            ConvB(in_channels=cf, out_channels=cf, kernel_size=1),
-            nn.ReLU(),
-            ConvB(in_channels=cf, out_channels=c * num_classes, kernel_size=1),
-        )
-
-    def sample(self):
-        with torch.no_grad():
-            sample = torch.zeros(1, self.dc, *self.input_shape).to(self.device)
-
             for i in range(self.input_shape[0]):
                 for j in range(self.input_shape[1]):
                     for c in range(self.dc):
-                        probs = self.predict_proba(sample)[0, :, c, i, j].cpu().numpy()
-                        sample[0, c, i, j] = torch.tensor(np.random.choice(self.num_classes, p=probs), dtype=int)
+                        probs = self.predict_proba(sample)[..., c, i, j]
+                        sample[:, c, i, j] = torch.multinomial(probs, 1).flatten()
 
-        return sample[0].cpu().numpy().transpose(1, 2, 0)
+        return sample.cpu().numpy().transpose(0, 2, 3, 1)
