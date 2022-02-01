b580c8662e33d3138ef8865b0cdee6a4c4d5c09a
Artem Kotov <breengles@gmail.com>
2022-02-01 19:42:03 +0300

    1st part done

--- utils/hw9_model.py
+++ utils/hw9_model.py
@@ -1,9 +1,8 @@
+import numpy as np
 import torch
 from torch import nn
-from torch.distributions import Normal
 from torch.optim import Adam
-from tqdm.auto import tqdm, trange
-import numpy as np
+from tqdm.auto import trange
 
 
 class Buffer:
@@ -44,10 +43,9 @@ class Buffer:
 
 
 class EBM(nn.Module):
-    def __init__(self, alpha=0.1, sig=1.6, buffer_size=8192, buffer_device="cpu"):
+    def __init__(self, alpha=0.1, buffer_size=8192, buffer_device="cpu"):
         super().__init__()
         self.alpha = alpha
-        self.sig = sig
 
         self.main = nn.Sequential(
             nn.Conv2d(1, 16, 5, 2, 4),
@@ -64,7 +62,6 @@ class EBM(nn.Module):
             nn.Linear(64, 1),
         )
 
-        self.dist = Normal(torch.Tensor([0]), torch.Tensor([sig]))
         self.buffer = Buffer(buffer_size, buffer_device)
 
     @property
@@ -74,7 +71,14 @@ class EBM(nn.Module):
     def forward(self, x):
         return self.main(x)
 
+    def set_model_grads(self, requires_grad=True):
+        for p in self.parameters():
+            p.requires_grad = requires_grad
+
     def langevin_sample(self, batch_size, K=60, eps_init=10.0, noise=0.005):
+        self.set_model_grads(False)
+        self.eval()
+
         buffer_part_size = int(batch_size * 0.95)
         x0_buffer = self.buffer.sample(buffer_part_size).to(self.device)
         x0_noise = torch.FloatTensor(batch_size - buffer_part_size, 1, 28, 28).uniform_(-1.0, 1.0).to(self.device)
@@ -83,34 +87,38 @@ class EBM(nn.Module):
         x.requires_grad = True
 
         for i in range(K):
-            eps = eps_init - eps_init * i / (K - 1)
-            z = (torch.randn_like(x) * np.sqrt(noise)).to(self.device)
+            eps = eps_init - eps_init * i / K
+            z = (torch.randn_like(x) * noise).to(self.device)
 
-            grad_x = torch.autograd.grad(self.main(x).sum(), x)[0].clip(-0.03, 0.03)
+            grad_x = torch.autograd.grad(self(x).sum(), x)[0].clip(-0.03, 0.03)
 
-            x = torch.clip(x + np.sqrt(2 * eps) * z - eps * grad_x, -1.0, 1.0)
+            x = torch.clip(x + np.sqrt(2 * eps) * z + eps * grad_x, -1.0, 1.0)
 
         self.buffer.push(x.detach())
+
+        self.set_model_grads(True)
+        self.train()
 
         return x
 
     def _step(self, batch, noise=0.005):
-        x_real = batch + torch.randn_like(batch) * np.sqrt(noise)
-        loss_real = self(x_real.to(self.device))
-
+        x_real = batch + torch.randn_like(batch) * noise
         x_fake = self.langevin_sample(batch.shape[0], noise=noise)
+
+        loss_real = self(x_real.to(self.device))
         loss_fake = self(x_fake.to(self.device))
 
-        regloss = self.alpha * (loss_real ** 2 + loss_fake ** 2).mean()
+        contrastive_loss = loss_fake.mean() - loss_real.mean()
+        reg_loss = self.alpha * (loss_real ** 2 + loss_fake ** 2).mean()
 
-        return (loss_real - loss_fake).mean(), regloss
+        return contrastive_loss, reg_loss
 
     def fit(self, trainloader, epochs=20, lr=1e-3, beta1=0.0, beta2=0.999):
         optim = Adam(self.parameters(), lr=lr, betas=(beta1, beta2))
         losses = {"contrastive": [], "regularization": []}
 
         for _ in trange(epochs, desc="Training...", leave=False):
-            for i, batch in enumerate(trainloader):
+            for batch in trainloader:
                 images = batch[0]
                 contrastive_loss, reg_loss = self._step(images)
                 loss = contrastive_loss + reg_loss
